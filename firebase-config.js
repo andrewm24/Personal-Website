860cc094c68e4a58439f3c78548836575628a015
Andrew Makarevich <andrew71865@gmail.com>
2026-04-11 23:37:56 -0400

    Remove Firebase config values and add Pages deploy workflow

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.ANDREW_PORTFOLIO_FIREBASE = {
-  apiKey: "AIzaSyDFBeQNfvdmphjOMrwk7AvySmrOKEKiczY",
-  authDomain: "personal-website-7022f.firebaseapp.com",
-  projectId: "personal-website-7022f",
-  storageBucket: "personal-website-7022f.firebasestorage.app",
-  messagingSenderId: "385576313613",
-  appId: "1:385576313613:web:7ae8e96b30e944dd6f0644",
-  measurementId: "G-CVHW0TP872"
+  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
+  authDomain: "REPLACE_WITH_FIREBASE_AUTH_DOMAIN",
+  projectId: "REPLACE_WITH_FIREBASE_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "REPLACE_WITH_FIREBASE_APP_ID",
+  measurementId: "REPLACE_WITH_FIREBASE_MEASUREMENT_ID"
 };
